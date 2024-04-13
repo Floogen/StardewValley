0f5bc0db6d1ed620dbef7b3245d073acf421d14c
Floogen <31755155+Floogen@users.noreply.github.com>
2024-04-13 01:05:27 -0500

    Now trimming titles and author list if they are too long

--- Common/Models/ContentPack.cs
+++ Common/Models/ContentPack.cs
@@ -12,5 +12,25 @@
         public DateTime CreatedTimestamp { get; set; }
         public string? ImagePath { get; set; }
         public string? ModUrl { get; set; }
+
+        public string? GetModNameForTitle()
+        {
+            if (ModName is null)
+            {
+                return null;
+            }
+
+            return ModName.Length >= 70 ? $"{ModName.Substring(0, 70)}..." : ModName;
+        }
+
+        public string? GetAuthorForTitle()
+        {
+            if (Author is null)
+            {
+                return null;
+            }
+
+            return Author.Length >= 25 ? $"{Author.Substring(0, 25)}..." : Author;
+        }
     }
 }
